666d91815aecb8c9f923a52c40ccab5af081a52a
athirathazhekkat <athirathazhekkat@gmail.com>
2026-04-10 11:14:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Hello, Streamlit!")
+st.write("This is a simple Streamlit app.")
+st.header("Features")
+st.subheader("Easy to Use")
+st.markdown("- Streamlit allows you to create web apps with just a few lines of code.")
+st.markdown("<h1>Im HTML Code</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='color:red;'>Im HTML & CSS Code</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='color:blue; font-size: 100px;'>Im HTML & CSS Code</h1>", unsafe_allow_html=True)
